b569d847e879222b4cdac76d7dd40cb18b06d094
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2016-08-29 23:06:04 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pybotics',
+    version='0.1.0',
+    packages=['examples', 'pybotics', 'pybotics.ut'],
+    url='https://github.com/nnadeau/pybotics',
+    license='MIT',
+    author='Nicholas Nadeau',
+    author_email='',
+    description='Python Toolbox for Robotics'
+)
