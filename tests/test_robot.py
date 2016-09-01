93c4cfc68ccbe1272b30963c3636219e3bd2d4ac
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2016-09-01 10:04:40 -0400

    added tests

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from pybotics import robot_model
 from pybotics.robot import Robot
+import numpy as np
 
 
 class TestRobot(TestCase):
@@ -10,10 +11,24 @@ class TestRobot(TestCase):
         self.robot.robot_model = robot_model.ur10()
 
     def test_num_dof(self):
+        # ur10 has 6 DOF
         assert self.robot.num_dof() == 6
 
     def test_fk(self):
-        self.fail()
+        transform = self.robot.fk()
+
+        # assert 4x4 transform
+        assert transform.shape[0] == 4
+        assert transform.shape[1] == 4
+        assert transform.size == 16
 
     def test_impair_robot_model(self):
-        self.fail()
+        impaired_robot = Robot()
+        impaired_robot.robot_model = robot_model.ur10()
+        impaired_robot.impair_robot_model(0.1)
+
+        model_diff = impaired_robot.robot_model - self.robot.robot_model
+
+        for link_parameters in model_diff:
+            for parameter in link_parameters:
+                assert abs(parameter) > 1e-9
