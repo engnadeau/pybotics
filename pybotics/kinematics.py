15127e66141fae76fef9a0e160a2613fd3ce8f03
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2016-09-09 10:01:02 -0400

    added support for angle units choice and changed order of input variables

--- pybotics/kinematics.py
+++ pybotics/kinematics.py
@@ -2,24 +2,28 @@ import math
 import numpy as np
 
 
-def forward_transform(rx, tx=None, tz=None, rz=None):
+def forward_transform(rx, tx=None, rz=None, tz=None, is_radians=True):
     """
     Returns the Denavit-Hartenberg Modified 4x4 matrix for a robot link (Craig 1986).
     Angular arguments are in radians.
     Calling forward_transform(rx,tx,tz,rz) is the same as using rotx(rx)*transl(tx,0,tx)*rotz(rz)
 
-    :param rx:
-    :param tx:
-    :param tz:
-    :param rz:
+    :param rx: DHM alpha
+    :param tx: DHM a
+    :param rz: DHM theta
+    :param tz: DHM d
     :return:
     """
 
     # if argument is a vector
     if tx is None:
-        [rx, tx, tz, rz] = rx
+        [rx, tx, rz, tz] = rx
 
     # get angular elements
+    if not is_radians:
+        rx = np.deg2rad(rx)
+        rz = np.deg2rad(rz)
+
     crx = math.cos(rx)
     srx = math.sin(rx)
     crz = math.cos(rz)
