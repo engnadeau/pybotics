2f6ed7553620aaf9bf57e1e6e6be9bfe230dba2f
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2016-12-31 16:17:59 -0500

    added tests

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,10 +1,14 @@
 from unittest import TestCase
 from pybotics.robot import Robot
+from pybotics import geometry
 import numpy as np
+import itertools
 
 
 class TestRobot(TestCase):
     def setUp(self):
+        np.set_printoptions(suppress=True)
+
         # load UR10 model for testing
         model = np.array([
             0, 0, 0, 118,
@@ -74,3 +78,92 @@ class TestRobot(TestCase):
 
         self.assertTrue(self.robot.validate_joint_angles(test_joints_list[0]))
         self.assertFalse(self.robot.validate_joint_angles(test_joints_list[1]))
+
+    def test_set_tool_xyz(self):
+        test_tool = [1.1, 2.2, 3.3]
+        self.robot.set_tool_xyz(test_tool)
+
+        for i in range(3):
+            self.assertAlmostEqual(test_tool[i], self.robot.tool[i, -1])
+
+    def test_generate_optimization_vector(self):
+        mask = self.robot.generate_optimization_mask()
+        vector = self.robot.generate_optimization_vector(mask)
+        self.assertEqual(len(vector), 0)
+
+        mask = self.robot.generate_optimization_mask(world_mask=True)
+        vector = self.robot.generate_optimization_vector(mask)
+        self.assertEqual(len(vector), 6)
+
+    def test_apply_optimization_vector(self):
+        # test full mask
+        mask = self.robot.generate_optimization_mask(world_mask=True,
+                                                     robot_model_mask=True,
+                                                     tool_mask=True,
+                                                     joint_stiffness_mask=True)
+        vector = self.robot.generate_optimization_vector(mask)
+        vector = [x + 1 for x in vector]
+        self.robot.apply_optimization_vector(vector, mask)
+
+        parameters = list(itertools.chain(
+            geometry.pose_2_xyzrpw(self.robot.world_frame),
+            self.robot.robot_model.ravel(),
+            geometry.pose_2_xyzrpw(self.robot.tool),
+            self.robot.joint_stiffness
+        ))
+
+        self.assertIsNone(np.testing.assert_allclose(parameters, vector))
+
+    def test_generate_optimization_mask(self):
+        mask = self.robot.generate_optimization_mask()
+        self.assertEqual(sum(mask), 0)
+
+        mask = self.robot.generate_optimization_mask(world_mask=True)
+        self.assertEqual(sum(mask), 6)
+
+        self.assertEqual(len(self.robot.generate_optimization_mask()),
+                         len(self.robot.generate_optimization_mask(world_mask=True,
+                                                                   robot_model_mask=True,
+                                                                   tool_mask=True,
+                                                                   joint_stiffness_mask=True)))
+
+        with self.assertRaises(AssertionError):
+            self.robot.generate_optimization_mask(world_mask=[True])
+        with self.assertRaises(AssertionError):
+            self.robot.generate_optimization_mask(robot_model_mask=[True])
+        with self.assertRaises(AssertionError):
+            self.robot.generate_optimization_mask(tool_mask=[True])
+        with self.assertRaises(AssertionError):
+            self.robot.generate_optimization_mask(joint_stiffness_mask=[True])
+
+    def test_generate_parameter_bounds(self):
+        mask = self.robot.generate_optimization_mask()
+        bounds = self.robot.generate_parameter_bounds(mask)
+        self.assertEqual(len(bounds), 0)
+
+        mask = self.robot.generate_optimization_mask(world_mask=True,
+                                                     robot_model_mask=True,
+                                                     tool_mask=True,
+                                                     joint_stiffness_mask=True)
+        bounds = self.robot.generate_parameter_bounds(mask)
+        self.assertTrue(len(bounds) > 0)
+        for bound in bounds:
+            for b in bound:
+                self.assertIsNone(b)
+
+    def test_jacobian_world(self):
+        # TODO: test
+        pass
+
+    def test_jacobian_flange(self):
+        # TODO: test
+        pass
+
+    def test_ik_fit_func(self):
+        reference_frame = np.eye(4)
+        test_angles = [0] * self.robot.num_dof()
+        test_pose = self.robot.fk(test_angles, reference_frame=reference_frame)
+
+        result_errors = self.robot.ik_fit_func(joint_angles=test_angles, pose=test_pose,
+                                               reference_frame=reference_frame)
+        self.assertAlmostEqual(sum(result_errors), 0, 1e-9)
