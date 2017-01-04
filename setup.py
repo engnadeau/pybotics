040078e0cc74387c17c4edc43ed07cb2c0ac3f09
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2017-01-04 18:59:50 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pybotics',
-    version='0.1.0',
+    version='0.1.3',
     packages=['examples', 'pybotics', 'tests'],
     url='https://github.com/nnadeau/pybotics',
     license='MIT',
