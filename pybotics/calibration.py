e96308e678cb384482f656c477c45095eacd0a29
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2017-01-20 11:50:02 -0500

    added typing and docstring; removed fitness function as this is part of implementation

--- pybotics/calibration.py
+++ pybotics/calibration.py
@@ -1,13 +1,28 @@
 import numpy as np
-from pybotics.exceptions import *
+from pybotics.exceptions import PybotException
+from pybotics import Robot
+from typing import Iterable, Sequence
 
 
-def compute_absolute_errors(robot, joints, torques, positions, reference_frame):
+def compute_absolute_errors(robot: Robot,
+                            joints: Sequence[Iterable[float]],
+                            torques: Sequence[Iterable[float]],
+                            positions: Sequence[Iterable[float]]) -> np.ndarray:
+    """Compute absolute forward kinematic position errors.
+
+    All positions must be in the same world frame as the robot.
+    Must have the same number of joints, torques, and positions.
+
+    :param robot: Robot object
+    :param joints: iterable of joint angles [rads]
+    :param torques: iterable of joint torques [Nm]
+    :param positions: iterable of the expected xyz positions [mm]
+    :return: absolute distance errors [mm]
+    """
     if len(joints) != len(torques) or len(joints) != len(positions):
         raise PybotException
 
     errors = []
-    robot.world_frame = reference_frame
     for i, _ in enumerate(joints):
         robot.joint_angles = joints[i]
         robot.joint_torques = torques[i]
@@ -21,40 +36,3 @@ def compute_absolute_errors(robot, joints, torques, positions, reference_frame):
 
     errors = np.abs(errors)
     return errors
-
-
-def calibration_fitness_func(optimization_vector,
-                             optimization_mask,
-                             robot,
-                             joints,
-                             torques,
-                             positions,
-                             reference_frame,
-                             calibration_type,
-                             return_type
-                             ):
-    # validate input
-    assert len(joints) == len(torques)
-    assert len(joints) == len(positions)
-
-    # make sure optimization_vector is a list
-    if isinstance(optimization_vector, np.ndarray):
-        optimization_vector = optimization_vector.tolist()
-
-    robot.apply_optimization_vector(optimization_vector, optimization_mask)
-
-    if calibration_type == 'abs':
-        errors = compute_absolute_errors(robot, joints, torques, positions, reference_frame)
-    elif calibration_type == 'rel':
-        # TODO: implement relative errors
-        # errors = compute_relative_errors(robot, joints, torques, positions)
-        pass
-    else:
-        raise PybotException
-
-    if return_type == 'list':
-        return errors
-    elif return_type == 'sumsq':
-        return np.sum(np.square(errors))
-    else:
-        raise PybotException
