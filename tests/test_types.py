fd48bda18bac9fe5380205ba1333af0d4066ae82
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2017-01-22 13:30:09 -0500

    Revert "want to be able to test for Vector type"
    This reverts commit 93f09d784a73adcdccef3a77cf8fdc2d12ce4518.

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,14 +1,8 @@
 import numpy as np
+from typing import List
+
 from pybotics.types import Vector
 
 
 def test_vector():
-    x = [1.1, 2.2, 3.3]
-    assert isinstance(x, Vector)
-
-    x = np.array([1.1, 2.2, 3.3])
-    assert isinstance(x, Vector)
-
-    x = (1.1, 2.2, 3.3)
-    assert isinstance(x, Vector)
     pass
