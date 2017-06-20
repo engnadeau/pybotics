075d486c11c0dbd3a3c81440ccb12ff788bd120f
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2017-06-20 10:50:17 -0400

    parsing git branch to append commit sha to branch versions when uploading to testPyPI

--- update_version.py
+++ update_version.py
@@ -10,9 +10,23 @@ root_path = os.path.dirname(__file__)
 logging.info('Root path:\t{}'.format(root_path))
 
 repo = git.Repo(root_path)
-tag = repo.tags[-1]
-version = tag.name
-logging.info('Latest git tag:\t{}'.format(version))
+latest_tag = repo.tags[-1]
+current_branch = repo.active_branch
+sha = repo.head.object.hexsha
+short_sha = repo.git.rev_parse(sha, short=4)
+
+logging.info('Repo:\t{}'.format(repo))
+logging.info('Latest tag:\t{}'.format(latest_tag))
+logging.info('Current branch:\t{}'.format(current_branch))
+logging.info('Last commit sha:\t{}'.format(sha))
+logging.info('Last commit short sha:\t{}'.format(short_sha))
+
+if str(current_branch) == 'master':
+    version = '{}'.format(latest_tag)
+else:
+    version = '{}+{}'.format(latest_tag, short_sha)
+
+logging.info('Package version:\t{}'.format(version))
 
 output_file = os.path.join(root_path, 'VERSION')
 logging.info('Writing version info to:\t{}'.format(output_file))
