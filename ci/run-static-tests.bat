3ab11c4cf0f245fc19d7cedb66942305f43cd409
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2017-10-28 15:01:42 -0400

    removed bat, as code duplication with bash script

--- ci/run-static-tests.bat
+++
@@ -1,9 +0,0 @@
-python -V
-echo PWD: %cd%
-
-mypy --strict pybotics
-flake8 --show-source -v --count --statistics pybotics
-vulture --exclude=docs,conftest.py,__init__.py .
-bandit -r -v pybotics
-pydocstyle -e -s -v pybotics
-pipdeptree -w fail -p pybotics
