d70a0f13877a97040c828c6bb7744d05a80eebe2
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2017-10-28 15:02:17 -0400

    flake8 now configured through setup.cfg; docstring plugin added

--- ci/run-static-tests.sh
+++ ci/run-static-tests.sh
@@ -4,8 +4,7 @@ python -V
 echo PWD: $PWD
 
 mypy --strict pybotics
-flake8 --show-source -v --count --statistics pybotics
+flake8
 vulture --exclude=docs,conftest.py,__init__.py .
 bandit -r -v pybotics
-pydocstyle -e -s -v pybotics
 pipdeptree -w fail -p pybotics
