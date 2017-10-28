d1227d9b8116b8ec560bc0091521b8ae04e8ad5c
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2017-10-28 15:02:40 -0400

    added calibration

---
+++ pybotics/calibration.py
@@ -0,0 +1,42 @@
+"""Calibration module."""
+from typing import Sequence
+import numpy as np  # type: ignore
+
+from pybotics.constants import POSITION_VECTOR_LENGTH
+from pybotics.errors import SequenceLengthError, ShapeMismatchError
+from pybotics.robot import Robot
+
+
+def compute_absolute_errors(robot: Robot,
+                            link_positions: Sequence[Sequence[float]],
+                            measured_positions: Sequence[Sequence[float]]
+                            ) -> np.ndarray:
+    """
+    Compute the absolute errors of a given set of positions.
+
+    :param robot: robot model
+    :param link_positions:
+    :param measured_positions:
+    :return:
+    """
+    # ensure ndarray
+    link_positions = np.array(link_positions)
+    measured_positions = np.array(measured_positions)
+
+    # validate
+    # TODO: remove type ignore when mypy supports numpy
+    if link_positions.shape[0] != measured_positions.shape[0]:  # type: ignore
+        raise ShapeMismatchError('link_positions.shape[0]',
+                                 'measured_positions.shape[0]')
+    if link_positions.shape[1] != robot.num_dof:  # type: ignore
+        raise SequenceLengthError('link_positions.shape[1]', robot.num_dof)
+    if measured_positions.shape[1] != POSITION_VECTOR_LENGTH:  # type: ignore
+        raise SequenceLengthError('measured_positions.shape[1]',
+                                  POSITION_VECTOR_LENGTH)
+
+    actual_poses = np.array(list(map(robot.fk, link_positions)))
+    actual_positions = actual_poses[:, :-1, -1]
+    position_errors = measured_positions - actual_positions
+    distance_errors = np.linalg.norm(position_errors, axis=1)
+
+    return distance_errors
--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -1,1 +1,40 @@
 """Calibration test."""
+import numpy as np
+
+from pybotics.calibration import compute_absolute_errors
+
+
+def test_compute_absolute_errors(serial_robot):
+    """
+    Test compute_absolute_errors().
+
+    :param serial_robot:
+    :return:
+    """
+    num_measurements = 10
+    joints = np.deg2rad([1, 2, 3, 4, 4, 6])
+    joints = np.tile(joints, (num_measurements, 1))
+
+    desired_positions = np.array([-1165.360706, -276.255829, -68.482801])
+    desired_positions = np.tile(desired_positions, (num_measurements, 1))
+
+    # perfect fk
+    errors = compute_absolute_errors(serial_robot, joints, desired_positions)
+    np.testing.assert_allclose(errors, np.zeros(num_measurements), atol=1e-6)
+
+    # bad fk
+    distance_offset = 10
+    serial_robot.world_frame.position = [distance_offset, 0, 0]
+    errors = compute_absolute_errors(serial_robot, joints, desired_positions)
+    np.testing.assert_allclose(errors,
+                               distance_offset * np.ones(num_measurements),
+                               atol=1e-6)
+
+    # bad fk
+    distance_offset = [10, 10, 10]
+    serial_robot.world_frame.position = distance_offset
+    errors = compute_absolute_errors(serial_robot, joints, desired_positions)
+    np.testing.assert_allclose(errors,
+                               np.linalg.norm(distance_offset) * np.ones(
+                                   num_measurements),
+                               atol=1e-6)
