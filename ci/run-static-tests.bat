c006b4394acf69b6407a6f850a28eb7fe3c7e836
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2017-11-08 16:03:30 -0500

    added windows static testing helper

---
+++ ci/run-static-tests.bat
@@ -0,0 +1,20 @@
+:: type checking
+mypy --strict pybotics
+
+:: linters
+flake8
+pylint pybotics
+vulture --exclude=docs,conftest.py,__init__.py .
+
+:: quality
+xenon --max-absolute B --max-modules A --max-average A pybotics
+
+:: security & dependencies
+pipdeptree -w fail -p pybotics
+bandit -r -v pybotics
+safety check --full-report ^
+    -r requirements/coverage.txt ^
+    -r requirements/examples.txt ^
+    -r requirements/packaging.txt ^
+    -r requirements/requirements.txt ^
+    -r requirements/static-testing.txt
