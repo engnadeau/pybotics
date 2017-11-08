b18328d54d8315b53a8768ad62c84558a9475c31
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2017-11-08 14:37:35 -0500

    updated ci and static testing

--- ci/run-static-tests.sh
+++ ci/run-static-tests.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+# type checking
 mypy --strict pybotics
+
+# linters
 flake8
 pylint pybotics
 vulture --exclude=docs,conftest.py,__init__.py .
-bandit -r -v pybotics
+
+# quality
+xenon --max-absolute B --max-modules A --max-average A pybotics
+
+# security & dependencies
 pipdeptree -w fail -p pybotics
+bandit -r -v pybotics
+safety check --full-report \
+    -r requirements/coverage.txt \
+    -r requirements/examples.txt \
+    -r requirements/packaging.txt \
+    -r requirements/requirements.txt \
+    -r requirements/static-testing.txt
