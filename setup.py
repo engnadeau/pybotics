55243fdf71b8f307cac7fab42ef757e4d59435d3
Nicholas Nadeau <nicholas@rogue-research.com>
2017-12-14 14:45:20 -0500

    using pathlib

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """Setup module."""
+from pathlib import Path
 from setuptools import setup, find_packages
-import os
 import logging
 from pybotics import __version__
 
@@ -10,22 +10,19 @@ if __name__ == '__main__':
     # version
     logging.info('Version: {}'.format(__version__))
 
+    # paths
+    current_dir = Path(__file__).parent
+    logging.info('Current dir: {}'.format(current_dir))
+
     # requirements
-    path = os.path.join(
-        os.path.dirname(__file__),
-        'requirements',
-        'requirements.txt'
-    )
+    path = current_dir / 'requirements' / 'requirements.txt'
     logging.info('Requirements path: {}'.format(path))
-    with open(path) as f:
+    with open(str(path)) as f:
         requirements = f.read().splitlines()
     logging.info('Requirements: {}'.format(requirements))
 
     # description
-    path = os.path.join(
-        os.path.dirname(__file__),
-        'README.md'
-    )
+    path = current_dir / 'README.md'
     logging.info('Requirements path: {}'.format(path))
     with open(path, encoding='utf-8') as f:
         description = f.read()
