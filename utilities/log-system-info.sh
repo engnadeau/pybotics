f9a836f7042d69711e8967aa3aa3c016d7aa8393
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2018-01-06 21:10:10 -0500

    removing obsolete

--- utilities/log-system-info.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-python -V
-echo "PWD: $PWD"
-pip show --files pybotics
--- utilities/test-ipython-examples.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-for F in "examples/*.ipynb"; do jupyter nbconvert --to script --execute --stdout $F | python; done
