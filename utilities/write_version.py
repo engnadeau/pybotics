0e1d991343cf250e3aa8587d935954d5ccd876df
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2018-01-06 21:13:42 -0500

    dumping version with setuptools_scm

---
+++ utilities/write_version.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from setuptools_scm import get_version
+import logging
+
+
+def main():
+    # get version
+    version = get_version(root='..', relative_to=__file__)
+    logging.info('Version: {}'.format(version))
+
+    # get root path
+    root_path = Path(__file__).parents[1]
+    logging.info('Root path: {}'.format(root_path.resolve()))
+
+    # dump version
+    version_path = root_path / 'VERSION'
+    logging.info('Dumping version to: {}'.format(version_path.resolve()))
+
+    with open(str(version_path), 'w') as f:
+        f.write(version)
+    logging.info('Version dumped')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
