4cb0078f81f28ca638f5d30eb4297bb7820dca88
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2018-01-06 21:15:03 -0500

    updated with new pipenv usage

--- utilities/run-static-tests.sh
+++ utilities/run-static-tests.sh
@@ -13,9 +13,5 @@ xenon --max-absolute B --max-modules A --max-average A pybotics
 # security & dependencies
 pipdeptree -w fail -p pybotics
 bandit -r -v pybotics
-safety check --full-report \
-    -r requirements/coverage.txt \
-    -r requirements/examples.txt \
-    -r requirements/packaging.txt \
-    -r requirements/requirements.txt \
-    -r requirements/static-testing.txt
+pipenv check pybotics
+pipenv check --unused pybotics
