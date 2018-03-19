c758bd32a556d8db4cf362356a01ece866fbaef9
Nicholas Nadeau <nicholas@rogue-research.com>
2018-03-19 17:04:28 -0400

    cleaned up

--- utilities/run-static-tests.sh
+++ utilities/run-static-tests.sh
@@ -1,35 +1,15 @@
 #!/usr/bin/env bash
 
-# type checking
-echo "mypy"
-mypy --strict pybotics
+set -e
+set -x
 
-# linters
-echo "flake8"
+mypy --strict .
 flake8
-
-echo "vulture"
 vulture --exclude=docs,conftest.py,__init__.py .
-
-# quality
-echo "xenon"
 xenon --max-absolute B --max-modules A --max-average A pybotics
-
-# security & dependencies
-echo "pipdeptree"
 pipdeptree -w fail -p pybotics
-
-echo "bandit"
 bandit -r -v pybotics
-
-echo "pipenv check"
 pipenv check pybotics
-
-echo "safety check"
 safety check --full-report \
-    -r requirements/main.txt \
-    -r requirements/example-testing.txt \
-    -r requirements/unit-testing.txt \
-    -r requirements/deployment.txt \
-    -r requirements/versioning.txt \
-    -r requirements/static-testing.txt
+    -r requirements.txt \
+    -r dev-requirements.txt
