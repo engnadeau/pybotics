fe50e6e6c7984cab59341ee676dc12641ff04cdd
Nicholas Nadeau <nicholas@aon3d.com>
2019-09-08 20:00:27 -0400

    cleaned init

--- pybotics/__init__.py
+++ pybotics/__init__.py
@@ -1,17 +1,1 @@
 """Pybotics modules."""
-from pkg_resources import DistributionNotFound, get_distribution
-
-# import modules
-from .kinematic_chain import KinematicChain
-from .robot import Robot
-from .tool import Tool
-
-# setup version info
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: no cover
-    # package is not installed
-    pass
-
-
-__all__ = ["KinematicChain", "Robot", "Tool"]
