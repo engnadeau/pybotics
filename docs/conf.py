ab1544d27adfe213e7f2902abc8e568803f27857
Nicholas Nadeau <nicholas@aon3d.com>
2019-09-13 08:10:48 -0400

    fixed formatting

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,9 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+import sys
+from pathlib import Path
+
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-from pathlib import Path
-import sys
 
 DOCS_DIR = Path(__file__).parent
 ROOT_DIR = DOCS_DIR.parent
