290ed5c81d8d360ce23b554ef8a41212864c7996
Nicholas Nadeau <nicholas@aon3d.com>
2019-09-13 07:54:37 -0400

    theme options

--- docs/conf.py
+++ docs/conf.py
@@ -68,6 +68,10 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
 
+# -- Theme configuration -------------------------------------------------
+
+html_logo = str((ROOT_DIR / "media" / "robotic-arm.svg").resolve())
+html_theme_options = {"canonical_url": "https://pybotics.readthedocs.io"}
 
 # -- Extension configuration -------------------------------------------------
 
