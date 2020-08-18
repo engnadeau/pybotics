4b3a65281a84a5ffd47b0c413e1136e0a4862af9
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2020-08-18 08:27:45 -0400

    chore(script): added helper script to bump dev deps

---
+++ scripts/update_dev_dependencies.py
@@ -0,0 +1,22 @@
+import toml
+from pathlib import Path
+import logging
+import subprocess
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    # get deps
+    path = Path.cwd() / "pyproject.toml"
+
+    logging.info(f"Loading {path.resolve()}")
+    with open(path) as f:
+        config = toml.load(f)
+
+    deps = config["tool"]["poetry"]["dev-dependencies"]
+
+    # process deps
+    for d in deps.keys():
+        logging.info(f"Updating {d} to latest")
+        args = ["poetry", "add", "--dev", f"{d}@latest"]
+        subprocess.run(args)
