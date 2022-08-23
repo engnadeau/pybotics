8f652bf8af6f1a47a3867f8dcc1ba1b42dcb599a
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2022-08-22 22:19:57 -0400

    fix: module typing

--- tests/test_tool.py
+++ tests/test_tool.py
@@ -9,11 +9,11 @@ def test_tool() -> None:
     """Test."""
     tool = Tool()
 
-    cg = [1, 2, 3]
+    cg = np.array([1, 2, 3])
     tool.cg = cg
-    np.testing.assert_allclose(tool.cg, cg)
+    np.testing.assert_allclose(tool.cg, cg)  # type: ignore
 
-    p = [1, 2, 3]
+    p = np.array([1, 2, 3])
     tool.position = p
-    np.testing.assert_allclose(tool.position, p)
-    np.testing.assert_allclose(tool.vector, matrix_2_vector(tool.matrix))
+    np.testing.assert_allclose(tool.position, p)  # type: ignore
+    np.testing.assert_allclose(tool.vector, matrix_2_vector(tool.matrix))  # type: ignore
