7cb5a3ca9c797e1d12cb049a2f8099610a0da499
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2022-08-23 13:46:46 -0400

    fix: typing

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -3,6 +3,7 @@ from pybotics.geometry import vector_2_matrix
 from pybotics.predefined_models import ur10
 from pybotics.robot import Robot
 from pybotics.tool import Tool
+import numpy as np
 
 
 def main() -> None:
@@ -16,11 +17,11 @@ def main() -> None:
 
     # add tool
     tool = Tool()
-    tool.position = [1, 2, 3]
+    tool.position = np.array([1, 2, 3])
     robot.tool = tool
 
     # set world frame
-    world_frame = vector_2_matrix([100, 200, 300, 0, 0, 0])
+    world_frame = vector_2_matrix(np.array([100, 200, 300, 0, 0, 0]))
     robot.world_frame = world_frame
 
     print(f"Robot: {robot}")
